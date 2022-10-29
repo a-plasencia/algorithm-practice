efc20d7c0206de9066d21c08ea16322ffd67cd20
Alex Plasencia <aPlasencia895@hotmail.com>
2022-10-29 05:55:56 +0000

    Completed the adjacentElementsProduct algorithm from code signal.

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 //Code Signal practice
 
 //Add function
-
 function add(param1, param2) {
   return param1 + param2;
 }
-
 console.log('2+1 is: ',add(1,2));
 
-
-//Psuedo code
-/**
+/*
  There are two constraints, year will be above 1 and below 2005, so it has to be a positive number
  input (year)
  output (integer)
@@ -34,3 +30,26 @@ function centuryFromYear(year) {
 }
 
 console.log('The century of the year 1905 is: ' ,centuryFromYear(1905));
+
+/*
+Create a function that loops through an array, and returns the largest product that the array can produce from the adjacent element
+Two guranteed constraints, the array is greater then 2 and less then 10, and the number is greater then -1000 and less then 1000
+input (array) [3,6,-2,-5,7,3]
+output (integer) [21]
+Declare a largest number variable to hold the product, assign it a value lower then the constraints of the algorithm(-1000).
+Loop through the array
+If the product of the adjacent element is greater then the largest number,
+assign the largest number that product.
+Continue to compare the two numbers until you reach the end of the array, then return the largestNumber variable
+*/
+function adjacentElementsProduct(inputArray) {
+  let largestNumber = -1001;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] * inputArray[i + 1] >= largestNumber && inputArray[i + 1] !== undefined) {
+      largestNumber = inputArray[i] * inputArray[i + 1];
+    }
+  }
+  return largestNumber;
+}
+const adjacentTestArray = [-2, 3, 5,1];
+console.log('The largest product in this array is: ', adjacentElementsProduct(adjacentTestArray));
