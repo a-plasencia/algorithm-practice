a4aaff24d37e8911286a14bd092f122a710ebe81
Alex Plasencia <aPlasencia895@hotmail.com>
2022-11-01 17:28:02 +0000

    Added the completed checkPalindrome algorithm from codesignal.

---
+++ src/checkPalindrome.js
@@ -0,0 +1,23 @@
+/*
+input (string)
+output (boolean)
+constraints:  The string is at least one character long and shorter then 10000
+from what I can gather also there are no spaces inbetween
+Create an empty array
+Go through the inputString backwards
+Push each letter into an array
+Use an array method to make it a string (join)
+Compare that string to the inputString, then return a boolean
+*/
+
+function checkPalindrome(inputString) {
+  const backwardsString = [];
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    backwardsString.push(inputString[i]);
+  }
+  if (inputString === backwardsString.join('')) {
+    return true;
+  } else {
+    return false;
+  }
+}
